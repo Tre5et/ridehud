40392da137d955269ba20148b6c004341bc5a312
TreSet <hanneshekl@gmail.com>
2025-08-22 11:06:32 +0200

    Fix: Fixed Tooltip position with no bars

--- src/main/java/net/treset/ridehud/mixin/InGameHudMixin.java
+++ src/main/java/net/treset/ridehud/mixin/InGameHudMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.render.RenderTickCounter;
 import net.treset.ridehud.config.Config;
 import net.treset.ridehud.entity_stats.VehicleStats;
 import net.treset.ridehud.VehicleHudRenderer;
+import net.treset.ridehud.entity_stats.VehicleStatsType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,41 +16,34 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class InGameHudMixin {
     @Inject(method = "render(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V", at = @At("HEAD"), cancellable = true)
     public void onRender(DrawContext ctx, RenderTickCounter tickCounter, CallbackInfo info) {
-
         VehicleHudRenderer.render(ctx);
     }
 
     @ModifyVariable(method = "renderHeldItemTooltip(Lnet/minecraft/client/gui/DrawContext;)V", at = @At(value = "STORE"), ordinal = 2)
     private int injectedInt(int k) {
-        int value = k;
-        if(VehicleStats.hasInstance()) {
-            // Move item tooltip over bars
-            if(Config.displayText.getBoolean()) {
-                value -= 18 + Config.barOffset.getInteger() + VehicleHudRenderer.getPlayerHealthOffset();
-            } else {
-                value -= 8 + Config.barOffset.getInteger() + VehicleHudRenderer.getPlayerHealthOffset();
-            }
-
-            // Fix creative mode item tooltip inconsistency
-            if(MinecraftClient.getInstance().interactionManager == null) return value;
-            if (!MinecraftClient.getInstance().interactionManager.hasStatusBars()) {
-                value -= 14;
-            }
-        }
-        return value;
+        return k - getTooltipBarOffset(true);
     }
 
     @ModifyConstant(method = "renderOverlayMessage(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V", constant = @Constant(intValue = 68))
     private int injectedArg(int k) {
-        int value = k;
-        if(VehicleStats.hasInstance()) {
-            // Move Overlay tooltips (notable "Press Shift to Dismount") over bars
-            if(Config.displayText.getBoolean()) {
-                value += 18 + Config.barOffset.getInteger() + VehicleHudRenderer.getPlayerHealthOffset();
-            } else {
-                value += 8 + Config.barOffset.getInteger() + VehicleHudRenderer.getPlayerHealthOffset();
-            }
+        return k + getTooltipBarOffset(false);
+    }
+
+    private static int getTooltipBarOffset(boolean fixCreativeModeOffset) {
+        if(!VehicleStats.hasInstance() || !(VehicleStats.getInstance().has(VehicleStatsType.SPEED) || VehicleStats.getInstance().has(VehicleStatsType.SPEED))) {
+            return 0;
         }
-        return value;
+
+        int offset = 8 + Config.barOffset.getInteger() + VehicleHudRenderer.getPlayerHealthOffset();
+        if(Config.displayText.getBoolean()) {
+            offset += 10;
+        }
+
+        // Fix creative mode item tooltip inconsistency
+        if(MinecraftClient.getInstance().interactionManager == null || !fixCreativeModeOffset) return offset;
+        if (!MinecraftClient.getInstance().interactionManager.hasStatusBars()) {
+            offset += 14;
+        }
+        return offset;
     }
 }
